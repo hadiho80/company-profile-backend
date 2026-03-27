9fb487b6c37a7f8741bb9fe173fc29153f71cd24
hadiho80 <45161732+hadiho80@users.noreply.github.com>
2026-03-27 10:48:16 +0800

    update cors for production

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      //   "https://company-profile-frontend.vercel.app",
+    ],
+  }),
+);
 app.use(express.json());
 
 // ✅ Middleware cek token
